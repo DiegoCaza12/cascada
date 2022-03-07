f7f5f166428f8185b6bba7acfe2641a23784a83c
DiegoCaza12 <99701829+DiegoCaza12@users.noreply.github.com>
2022-03-06 21:44:22 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { NavController } from '@ionic/angular';
 import { AccesoService } from 'src/app/servicios/acceso.service';
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
